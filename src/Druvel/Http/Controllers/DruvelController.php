fe2cfe14a3869b139328427aefcf274548f09fa9
lucapitzoi <luca.pitzoi@sindria.org>
2019-07-25 20:49:50 +0200

    Fix some

---
+++ src/Druvel/Http/Controllers/DruvelController.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Class Druvel_Http_Controllers_DruvelController
+ *
+ * @package druvel
+ * @author Luca Pitzoi <luca.pitzoi@sindria.org>
+ * @version 0.1.0
+ */
+class Druvel_Http_Controllers_DruvelController extends Druvel_Http_Controllers_Controller
+{
+    // code here
+}
