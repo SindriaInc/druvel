30f27d8f806a5ea19538a9ad25230ff9fcc3306c
lucapitzoi <luca.pitzoi@sindria.org>
2019-07-26 03:02:42 +0200

    Added debug helper file with manual var_dumper

---
+++ src/Druvel/Helpers/debug.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @file
+ * debug.php
+ *
+ * Manual var_dumper without symfony/var_dumper package
+ *
+ * @author Luca Pitzoi <luca.pitzoi@sindria.org>
+ * @version 0.1.0
+ *
+ */
+
+
+if (!function_exists('dd')) {
+    /**
+     * Dump and Die debug function
+     */
+    function dd() {
+        $args = func_get_args();
+        echo '<pre>';
+        call_user_func_array('var_dump', $args);
+        echo '</pre>';
+        die();
+    }
+}
+
+
+if (!function_exists('d')) {
+    /**
+     * Dump debug function
+     */
+    function d() {
+        $args = func_get_args();
+        echo '<pre>';
+        call_user_func_array('var_dump', $args);
+        echo '</pre>';
+    }
+}
--- src/Druvel/Helpers/helpers.php
+++ src/Druvel/Helpers/helpers.php
@@ -1662,34 +1662,6 @@ if (!function_exists('tap')) {
 }
 
 
-// Manual var_dumper without symfony/var_dumper package
-//if (!function_exists('dd')) {
-//    /**
-//     * Dump and Die debug function
-//     */
-//    function dd() {
-//        $args = func_get_args();
-//        echo '<pre>';
-//        call_user_func_array('var_dump', $args);
-//        echo '</pre>';
-//        die();
-//    }
-//}
-//
-//
-//if (!function_exists('d')) {
-//    /**
-//     * Dump debug function
-//     */
-//    function d() {
-//        $args = func_get_args();
-//        echo '<pre>';
-//        call_user_func_array('var_dump', $args);
-//        echo '</pre>';
-//    }
-//}
-
-
 if (!function_exists('data_fill')) {
     /**
      * Fill in data where it's missing.
